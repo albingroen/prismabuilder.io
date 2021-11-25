b41c530c9345addecfa9d48346025c0d1ced7524
Albin Groen <albin.groen@gmail.com>
2021-11-25 14:22:10 +0100

    :chart_with_upwards_trend: Add splitbee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "../styles/globals.css";
 import Seo from "../components/Seo";
+import splitbee from "@splitbee/web";
 import type { AppProps } from "next/app";
 import { LensProvider } from "@prisma/lens";
 import { SchemaContext } from "../lib/context";
 import { Toaster } from "react-hot-toast";
 import { useState } from "react";
+
+splitbee.init();
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [schema, setSchema] = useState({
