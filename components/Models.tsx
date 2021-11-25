c4b92721508953f172f6a468c257405c4fa8d578
Albin Groen <albin.groen@gmail.com>
2021-11-25 15:55:33 +0100

    :sparkles: Add creator and repo links

--- components/Models.tsx
+++ components/Models.tsx
@@ -18,63 +18,85 @@ export default function Models() {
 
   return (
     <div className="flex flex-col border flex-1 max-w-sm h-screen overflow-y-auto p-4 space-y-3 bg-gray-100">
-      {schema.models.map((model: Model, i: number) => (
-        <Link href={`/models/${i}`} key={model.name}>
-          <a>
-            <Card className="border border-transparent hover:border-blue-500 cursor-pointer transition flex items-center space-x-3">
-              <Layers size={20} className="text-gray-500" />
-              <h3>{model.name}</h3>
-            </Card>
-          </a>
-        </Link>
-      ))}
+      <div className="flex flex-col space-y-3 flex-1">
+        {schema.models.map((model: Model, i: number) => (
+          <Link href={`/models/${i}`} key={model.name}>
+            <a>
+              <Card className="border border-transparent hover:border-blue-500 cursor-pointer transition flex items-center space-x-3">
+                <Layers size={20} className="text-gray-500" />
+                <h3>{model.name}</h3>
+              </Card>
+            </a>
+          </Link>
+        ))}
 
-      {schema.models.length ? <Separator /> : null}
+        {schema.models.length ? <Separator /> : null}
 
-      <Button
-        onClick={() => {
-          if (schema.models.some((model: Model) => model.name === "New")) {
-            toast.error("A model called New already exists");
-          } else {
-            const newSchema = {
-              ...schema,
-              models: [
-                ...schema.models,
-                {
-                  name: "New",
-                  fields: [ID_FIELD],
-                  enums: [],
-                },
-              ],
-            };
-            setSchema(newSchema);
-            router.push(`/models/${newSchema.models.length - 1}`);
-          }
-        }}
-        variant="secondary"
-      >
-        New model
-      </Button>
-
-      {schema.models.length ? (
         <Button
           onClick={() => {
-            setShowingSchema(true);
+            if (schema.models.some((model: Model) => model.name === "New")) {
+              toast.error("A model called New already exists");
+            } else {
+              const newSchema = {
+                ...schema,
+                models: [
+                  ...schema.models,
+                  {
+                    name: "New",
+                    fields: [ID_FIELD],
+                    enums: [],
+                  },
+                ],
+              };
+              setSchema(newSchema);
+              router.push(`/models/${newSchema.models.length - 1}`);
+            }
           }}
+          variant="secondary"
         >
-          Generate schema
+          New model
         </Button>
-      ) : null}
 
-      {showingSchema && (
-        <Modal
-          onClose={() => setShowingSchema(false)}
-          open={showingSchema}
-          heading="Schema"
+        {schema.models.length ? (
+          <Button
+            onClick={() => {
+              setShowingSchema(true);
+            }}
+          >
+            Generate schema
+          </Button>
+        ) : null}
+
+        {showingSchema && (
+          <Modal
+            onClose={() => setShowingSchema(false)}
+            open={showingSchema}
+            heading="Schema"
+          >
+            <Schema />
+          </Modal>
+        )}
+      </div>
+
+      <div className="divide-x text-xs text-gray-600 justify-self-end">
+        <a
+          className="hover:underline hover:text-gray-700 pr-2"
+          href="https://albingroen.com"
+          rel="noopener noreferrer"
+          target="_blank"
         >
-          <Schema />
-        </Modal>
-      )}
+          Creator
+        </a>
+
+        <a
+          href="https://github.com/albingroen/prismabuilder.io"
+          className="hover:underline hover:text-gray-700 pl-2"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Source code
+        </a>
+      </div>
     </div>
   );
 }
