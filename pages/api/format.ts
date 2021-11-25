a3bea372775d51cd1c98177d05c92d23145afbb7
Albin Groen <albin.groen@gmail.com>
2021-11-25 14:41:28 +0100

    :recycle: Remove formatting

--- pages/api/format.ts
+++ pages/api/format.ts
@@ -1,11 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { formatSchema } from "@prisma/sdk";
 import { jsonToPrismaSchema } from "../../lib/prisma";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const schema = await formatSchema({ schema: jsonToPrismaSchema(req.body) });
+  const schema = jsonToPrismaSchema(req.body);
   res.json(schema);
 }
