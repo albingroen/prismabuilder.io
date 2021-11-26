7574427bff2809ce957921173227727b2e064b15
Albin Groen <albin.groen@gmail.com>
2021-11-26 08:16:38 +0100

    :sparkles: Improve schema component

--- components/Schema.tsx
+++ components/Schema.tsx
@@ -1,9 +1,12 @@
 import axios from "axios";
+import toast from "react-hot-toast";
+import { Loader } from "@prisma/lens";
 import { SchemaContext } from "../lib/context";
 import { useContext, useEffect, useState } from "react";
 
 const Schema = () => {
   const { schema } = useContext(SchemaContext);
+  const [loading, setLoading] = useState(true);
   const [result, setResult] = useState("");
 
   useEffect(() => {
@@ -11,10 +14,27 @@ const Schema = () => {
       .post("https://prismabuilder-io-api.onrender.com/format", { schema })
       .then((res) => {
         setResult(res.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        toast.error("Failed to generate schema");
+        setLoading(false);
       });
   }, [schema]);
 
-  return <pre>{result}</pre>;
+  return (
+    <div>
+      {result ? (
+        <pre>{result}</pre>
+      ) : (
+        loading && (
+          <div className="flex items-center justify-center h-64">
+            <Loader />
+          </div>
+        )
+      )}
+    </div>
+  );
 };
 
 export default Schema;
