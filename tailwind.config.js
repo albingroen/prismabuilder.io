37f96b2bf41cfcafe07e82eac29d65c3774ad756
Albin Groen <albin.groen@gmail.com>
2021-11-28 10:22:12 +0100

    chore: Use TailwindCSS JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media", // or 'media' or 'class'
+  mode: "jit",
   theme: {
     extend: {},
   },
