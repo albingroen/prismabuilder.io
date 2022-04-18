5a3c579fc32c73f8054bafb4d6c530fd606c140d
Albin Groen <albin.groen@gmail.com>
2022-04-18 21:00:29 +0200

    Fix menu items

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,19 +3,13 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+use tauri::{Menu, MenuItem, Submenu};
 
 fn main() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let close = CustomMenuItem::new("close".to_string(), "Close");
-    let minimize = CustomMenuItem::new("minimize".to_string(), "Minimize");
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-
     let app_submenu = Submenu::new("File", Menu::new()
-        .add_item(hide)
-        .add_item(minimize)
-        .add_item(close)
-        .add_item(quit)
+        .add_native_item(MenuItem::Hide)
+        .add_native_item(MenuItem::Minimize)
+        .add_native_item(MenuItem::Quit)
     );
 
     let edit_submenu = Submenu::new("Edit", Menu::new()
