d10557f4285ae3181668eb42963afbede49f08f0
Albin Groen <albin.groen@gmail.com>
2022-05-22 10:25:11 +0200

    Add drag and drop functionality for reordering fields

--- pages/schemas/[schemaId]/models/[id]/index.tsx
+++ pages/schemas/[schemaId]/models/[id]/index.tsx
@@ -4,13 +4,20 @@ import Tag from "../../../../../components/Tag";
 import UpdateField from "../../../../../components/UpdateField";
 import toast from "react-hot-toast";
 import { Button, Menu, Separator, TextField, Title } from "@prisma/lens";
-import { CheckSquare, Edit, MoreVertical, Trash2 } from "react-feather";
+import {
+  CheckSquare,
+  Edit,
+  MoreVertical,
+  Trash2,
+  Menu as MenuIcon,
+} from "react-feather";
 import { isFieldTypeEnum, TYPES } from "../../../../../lib/fields";
 import { prismaTypesToIcons } from "../../../../../lib/icons";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/dist/client/router";
 import { useSchemaContext } from "../../../../../lib/context";
 import { Field, FieldType, Model } from "../../../../../lib/types";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const Model = () => {
   const { schema, setSchema } = useSchemaContext();
@@ -174,92 +181,152 @@ const Model = () => {
             <Separator />
 
             <div className="flex space-x-8">
-              <div className="flex flex-col space-y-3 flex-1 model-fields p-1 overflow-y-auto">
-                {model?.fields
-                  ?.filter((field: Field) => {
-                    if (schema.database === "sqlite") {
-                      return !isFieldTypeEnum(field.type, schema.database);
-                    }
-                    return true;
-                  })
-                  .map((field: Field) => {
-                    const enumType = schema.enums.find(
-                      (e) => e.name === field.type && e.fields.length
+              <DragDropContext
+                onDragEnd={(result) => {
+                  const newFields = [...model.fields];
+
+                  if (result.destination) {
+                    newFields.splice(result.source.index, 1);
+
+                    newFields.splice(
+                      result.destination.index,
+                      0,
+                      model.fields[result.source.index]
                     );
 
-                    const Icon = enumType
-                      ? prismaTypesToIcons.Enum
-                      : field.relationField
-                      ? prismaTypesToIcons.Model
-                      : prismaTypesToIcons[field.type] ??
-                        prismaTypesToIcons.default;
+                    updateModel({
+                      fields: newFields,
+                    });
+                  }
+                }}
+              >
+                <Droppable droppableId={model.name} direction="vertical">
+                  {({ droppableProps, innerRef, placeholder }) => (
+                    <div
+                      {...droppableProps}
+                      ref={innerRef}
+                      className="flex flex-col flex-1 model-fields p-1 overflow-y-auto"
+                    >
+                      {model?.fields
+                        ?.filter((field: Field) => {
+                          if (schema.database === "sqlite") {
+                            return !isFieldTypeEnum(
+                              field.type,
+                              schema.database
+                            );
+                          }
+                          return true;
+                        })
+                        .map((field: Field, i) => {
+                          const enumType = schema.enums.find(
+                            (e) => e.name === field.type && e.fields.length
+                          );
 
-                    return (
-                      <button
-                        className="rounded-lg bg-white shadow-md text-left border border-transparent focus:border-blue-500 hover:border-blue-500 cursor-pointer transition py-3.5 px-4 flex items-center justify-between"
-                        onClick={() => setEditingField(field.name)}
-                        key={field.name}
-                      >
-                        <div className="flex items-center space-x-4">
-                          <div
-                            className={`rounded-md ${
-                              field.relationField || enumType
-                                ? "bg-indigo-100"
-                                : "bg-blue-100"
-                            } flex items-center justify-center p-4`}
-                          >
-                            <Icon
-                              className={`${
-                                field.relationField || enumType
-                                  ? "text-indigo-600"
-                                  : "text-blue-600"
-                              }`}
-                              size={24}
-                            />
-                          </div>
-                          <div className="flex flex-col space-y-1">
-                            <h4 className="text-lg w-52 font-medium">
-                              {field.name}
-                            </h4>
-                            <div className="flex items-center space-x-2">
-                              <Tag>
-                                {field.list ? "[" : ""}
-                                {field.type}
-                                {field.list ? "]" : ""}
-                              </Tag>
-                              {field.unique && <Tag>Unique</Tag>}
-                              {field.required && <Tag>Required</Tag>}
-                              {field.default && <Tag>{field.default}</Tag>}
-                              {field.isUpdatedAt && <Tag>Updated At</Tag>}
-                              {field.isId && <Tag>ID</Tag>}
-                            </div>
-                          </div>
-                        </div>
+                          const Icon = enumType
+                            ? prismaTypesToIcons.Enum
+                            : field.relationField
+                            ? prismaTypesToIcons.Model
+                            : prismaTypesToIcons[field.type] ??
+                              prismaTypesToIcons.default;
 
-                        <div
-                          role="button"
-                          onClick={(e) => {
-                            e.preventDefault();
-                            e.stopPropagation();
+                          return (
+                            <Draggable
+                              draggableId={field.name}
+                              key={field.name}
+                              index={i}
+                            >
+                              {(
+                                { dragHandleProps, draggableProps, innerRef },
+                                { isDragging }
+                              ) => (
+                                <button
+                                  {...draggableProps}
+                                  ref={innerRef}
+                                  className="mb-3 rounded-lg bg-white shadow-md text-left border border-transparent cursor-pointer py-3.5 px-4 flex items-center justify-between"
+                                  onClick={() => setEditingField(field.name)}
+                                >
+                                  <div className="flex items-center space-x-4">
+                                    <div {...dragHandleProps}>
+                                      <MenuIcon
+                                        size={20}
+                                        className={`${
+                                          isDragging
+                                            ? "text-gray-700"
+                                            : "text-gray-500 hover:text-gray-700"
+                                        } transition`}
+                                      />
+                                    </div>
 
-                            updateModel({
-                              fields: model.fields.filter(
-                                (f: Field) => f.name !== field.name
-                              ),
-                            });
-                          }}
-                          aria-label="Delete field"
-                          className="px-4"
-                        >
-                          <Trash2
-                            className="text-red-400 hover:text-red-600 transition"
-                            size={20}
-                          />
-                        </div>
-                      </button>
-                    );
-                  })}
-              </div>
+                                    <div
+                                      className={`rounded-md ${
+                                        field.relationField || enumType
+                                          ? "bg-indigo-100"
+                                          : "bg-blue-100"
+                                      } flex items-center justify-center p-4`}
+                                    >
+                                      <Icon
+                                        className={`${
+                                          field.relationField || enumType
+                                            ? "text-indigo-600"
+                                            : "text-blue-600"
+                                        }`}
+                                        size={24}
+                                      />
+                                    </div>
+                                    <div className="flex flex-col space-y-1">
+                                      <h4 className="text-lg w-52 font-medium">
+                                        {field.name}
+                                      </h4>
+                                      <div className="flex items-center space-x-2">
+                                        <Tag>
+                                          {field.list ? "[" : ""}
+                                          {field.type}
+                                          {field.list ? "]" : ""}
+                                        </Tag>
+                                        {field.unique && <Tag>Unique</Tag>}
+                                        {field.required && <Tag>Required</Tag>}
+                                        {field.default && (
+                                          <Tag>{field.default}</Tag>
+                                        )}
+                                        {field.isUpdatedAt && (
+                                          <Tag>Updated At</Tag>
+                                        )}
+                                        {field.isId && <Tag>ID</Tag>}
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div
+                                    role="button"
+                                    onClick={(e) => {
+                                      e.preventDefault();
+                                      e.stopPropagation();
+
+                                      updateModel({
+                                        fields: model.fields.filter(
+                                          (f: Field) => f.name !== field.name
+                                        ),
+                                      });
+                                    }}
+                                    aria-label="Delete field"
+                                    className="px-4"
+                                  >
+                                    <Trash2
+                                      className="text-red-400 hover:text-red-600 transition"
+                                      size={20}
+                                    />
+                                  </div>
+                                </button>
+                              )}
+                            </Draggable>
+                          );
+                        })}
+
+                      {placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </DragDropContext>
 
               <div className="flex-1 max-w-sm model-fields overflow-y-auto bg-gray-200 rounded-lg p-4 flex flex-col space-y-4">
                 <h2 className="font-semibold text-xl">Add field</h2>
