555f10f260843a0293f9029b2e1a130df1816608
Daniel Mateiu <daniel.mateiu@redbull.com>
2023-02-12 12:38:05 +0100

    refactor: extract manual schema editor component

--- components/ImportSchema.tsx
+++ components/ImportSchema.tsx
@@ -1,83 +1,17 @@
-import axios from "axios";
-import toast from "react-hot-toast";
-import { Button } from "@prisma/lens";
-import { Enum, Model } from "../lib/types";
-import { apiUrl } from "../lib/config";
-import { useSchemaContext } from "../lib/context";
-import { useState } from "react";
+import React from "react";
+import ManualSchemaEditor from "./ManualSchemaEditor";
 
 type ImportSchemaProps = {
   onClose: () => void;
 };
 
 const ImportSchema = ({ onClose }: ImportSchemaProps) => {
-  const { schema, setSchema } = useSchemaContext();
-
-  const [importSchemaLoading, setImportSchemaLoading] =
-    useState<boolean>(false);
-  const [importSchema, setImportSchema] = useState<string>("");
-
   return (
     <div>
       <p className="text-sm text-gray-700">
         Be wary that importing a schema will omit any default values on fields.
       </p>
-      <textarea
-        autoFocus
-        value={importSchema}
-        onChange={(e) => {
-          setImportSchema(e.target.value);
-        }}
-        className="font-mono h-96 w-full my-4 border rounded-lg resize-none p-4 whitespace-pre overflow-auto"
-      />
-      <Button
-        onPress={() => {
-          setImportSchemaLoading(true);
-          axios
-            .post(`${apiUrl}/parse`, { schema: importSchema })
-            .then((res) => {
-              const importedSchema = res.data;
-              if (
-                schema.models.some((model: Model) =>
-                  importedSchema.models
-                    .map((m: Model) => m.name)
-                    .includes(model.name)
-                )
-              ) {
-                toast.error("Some model has a colliding name");
-                setImportSchemaLoading(false);
-              } else if (
-                schema.enums.some((enumValue: Enum) =>
-                  importedSchema.enums
-                    .map((e: Enum) => e.name)
-                    .includes(enumValue.name)
-                )
-              ) {
-                toast.error("Some enum has a colliding name");
-                setImportSchemaLoading(false);
-              } else if (importedSchema.models?.length) {
-                setSchema({
-                  ...schema,
-                  models: [...schema.models, ...importedSchema.models],
-                  enums: [...schema.enums, ...(importedSchema.enums ?? [])],
-                });
-                setImportSchemaLoading(false);
-                setImportSchema("");
-                onClose();
-              } else {
-                toast.error("Make sure to import a full schema");
-                setImportSchemaLoading(false);
-              }
-            })
-            .catch(() => {
-              toast.error("Failed to import schema");
-              setImportSchemaLoading(false);
-            });
-        }}
-        isLoading={importSchemaLoading}
-      >
-        Import schema
-      </Button>
+      <ManualSchemaEditor onClose={onClose} />
     </div>
   );
 };
---
+++ components/ManualSchemaEditor.tsx
@@ -0,0 +1,82 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Button } from "@prisma/lens";
+import { Enum, Model } from "../lib/types";
+import { apiUrl } from "../lib/config";
+import { useSchemaContext } from "../lib/context";
+import React, { useState } from "react";
+
+type ImportSchemaProps = {
+  onClose: () => void;
+};
+
+const ManualSchemaEditor = ({ onClose }: ImportSchemaProps) => {
+  const { schema, setSchema } = useSchemaContext();
+
+  const [importSchemaLoading, setImportSchemaLoading] =
+    useState<boolean>(false);
+  const [importSchema, setImportSchema] = useState<string>("");
+
+  return (
+    <div>
+      <textarea
+        autoFocus
+        value={importSchema}
+        onChange={(e) => {
+          setImportSchema(e.target.value);
+        }}
+        className="font-mono h-96 w-full my-4 border rounded-lg resize-none p-4 whitespace-pre overflow-auto"
+      />
+      <Button
+        onPress={() => {
+          setImportSchemaLoading(true);
+          axios
+            .post(`${apiUrl}/parse`, { schema: importSchema })
+            .then((res) => {
+              const importedSchema = res.data;
+              if (
+                schema.models.some((model: Model) =>
+                  importedSchema.models
+                    .map((m: Model) => m.name)
+                    .includes(model.name)
+                )
+              ) {
+                toast.error("Some model has a colliding name");
+                setImportSchemaLoading(false);
+              } else if (
+                schema.enums.some((enumValue: Enum) =>
+                  importedSchema.enums
+                    .map((e: Enum) => e.name)
+                    .includes(enumValue.name)
+                )
+              ) {
+                toast.error("Some enum has a colliding name");
+                setImportSchemaLoading(false);
+              } else if (importedSchema.models?.length) {
+                setSchema({
+                  ...schema,
+                  models: [...schema.models, ...importedSchema.models],
+                  enums: [...schema.enums, ...(importedSchema.enums ?? [])],
+                });
+                setImportSchemaLoading(false);
+                setImportSchema("");
+                onClose();
+              } else {
+                toast.error("Make sure to import a full schema");
+                setImportSchemaLoading(false);
+              }
+            })
+            .catch(() => {
+              toast.error("Failed to import schema");
+              setImportSchemaLoading(false);
+            });
+        }}
+        isLoading={importSchemaLoading}
+      >
+        Import schema
+      </Button>
+    </div>
+  );
+};
+
+export default ManualSchemaEditor;
