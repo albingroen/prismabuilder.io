6b9f6cfa58f4d2fa03af73cc745baa933858c88e
Daniel Mateiu <daniel.mateiu@redbull.com>
2023-02-12 14:30:39 +0100

    feat: add possibility to switch between schema import mechanisms

--- components/ImportSchema.tsx
+++ components/ImportSchema.tsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import ManualSchemaEditor from "./ManualSchemaEditor";
 import FileUploadSchemaEditor from "./FileUploadSchemaEditor";
+import { Button } from "@prisma/lens";
 
 type ImportSchemaProps = {
   onClose: () => void;
 };
 
 const ImportSchema = ({ onClose }: ImportSchemaProps) => {
+  const [schemaSource, setSchemaSource] = useState("manual");
+
   return (
     <div>
-      <p className="text-sm text-gray-700">
-        Be wary that importing a schema will omit any default values on fields.
+      <p className="text-sm px-14 text-center text-gray-600">
+        Import your schema by typing or pasting your schema directly into the
+        editor provided or use the file upload option to browse your local file
+        system and select the file to import, if you have the schema saved as a
+        file.
       </p>
-      <ManualSchemaEditor onClose={onClose} />
-      <FileUploadSchemaEditor onClose={onClose} />
+
+      <div className="py-6 px-20 flex flex-row content-center justify-center gap-4">
+        <Button
+          fillParent={true}
+          onPress={() => setSchemaSource("manual")}
+          variant={schemaSource === "manual" ? "primary" : "secondary"}
+        >
+          Manual Editor
+        </Button>
+        <Button
+          fillParent={true}
+          onPress={() => setSchemaSource("fileUpload")}
+          variant={schemaSource === "fileUpload" ? "primary" : "secondary"}
+        >
+          File Upload
+        </Button>
+      </div>
+      {schemaSource === "manual" && <ManualSchemaEditor onClose={onClose} />}
+      {schemaSource === "fileUpload" && (
+        <FileUploadSchemaEditor onClose={onClose} />
+      )}
     </div>
   );
 };
